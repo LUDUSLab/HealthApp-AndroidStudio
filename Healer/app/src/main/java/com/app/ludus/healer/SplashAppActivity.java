94334bf4ad50c0c0f2a16b9ab37703ae4a324428
Hosomy17 <jackson.daniel.17dh@gmail.com>
2016-06-23 11:07:51 -0400

    First commit

---
+++ Healer/app/src/main/java/com/app/ludus/healer/SplashAppActivity.java
@@ -0,0 +1,13 @@
+package com.app.ludus.healer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_app);
+    }
+}
