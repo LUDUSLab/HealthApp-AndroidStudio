b2aa62477f04ed6a050c2e845ca3fdaaa23e8b88
YonHoshihara <hoshiharyon@gmail.com>
2016-06-23 19:17:05 -0700

    adição do view daily

--- Healer/app/src/main/java/com/app/ludus/healer/DailyActivity.java
+++ Healer/app/src/main/java/com/app/ludus/healer/DailyActivity.java
@@ -1,29 +1,45 @@
 package com.app.ludus.healer;
 
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class DailyActivity extends AppCompatActivity {
-
+    private ImageButton btn1,btn2,btn3;
+    private boolean tomou1,tomou2,tomou3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        tomou1=tomou2=tomou3=false;
+        btn1 = (ImageButton)findViewById(R.id.daily_btn_med1);
+        btn2 = (ImageButton)findViewById(R.id.daily_btn_med2);
+        btn3 = (ImageButton)findViewById(R.id.daily_btn_med3);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.daily_cst_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(!tomou1)
+                    btn1.setImageResource(R.drawable.pilula_verde);
+                else
+                    btn1.setImageResource(R.drawable.pilula_cinza);
+                tomou1 = tomou1 && false;
+
             }
         });
+
     }
 
 }
--- Healer/app/src/main/java/com/app/ludus/healer/MainActivity.java
+++ Healer/app/src/main/java/com/app/ludus/healer/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_paciente);
-        startActivity(new Intent(getApplicationContext(),MedicamentoActivity.class));
+        startActivity(new Intent(getApplicationContext(),DailyActivity.class));
         finish();
     }
 
